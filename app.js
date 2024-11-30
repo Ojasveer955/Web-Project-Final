264ced7e1ea8cb03a873d636b4c503bee72045f9
Disha Satija <disha.satija.23cse@bmu.edu.in>
2024-11-30 21:49:47 +0530

    template engines setup...?

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-// Configure Handlebars
-app.engine('handlebars', exphbs);
+// Set up Handlebars for views
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+// Set up EJS for footer
+app.engine('ejs', require('ejs').__express);
 
 // Define static folder
 app.use(express.static('public'));
 
+// Route to render the page
+app.get('/', (req, res) => {
+  // Render the Handlebars header and content
+  res.render('home', {
+    layout: 'main', // Use the main layout
+    footer: 'footer', // Include footer template
+  });
+});
+
+// Start the server
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
