f22bbf0aa2af6207da07821d96a8c10501c519fc
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2024-11-30 23:04:23 +0530

    test

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+// Add middleware to parse JSON bodies
+app.use(express.json());
+
 // Set up Handlebars for views
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -13,6 +16,9 @@ app.engine('ejs', require('ejs').__express);
 // Define static folder
 app.use(express.static('public'));
 
+// Import the bookController
+const bookController = require('./controller/bookController');
+
 // Route to render the page
 app.get('/', (req, res) => {
   // Render the Handlebars header and content
@@ -21,6 +27,13 @@ app.get('/', (req, res) => {
     footer: 'footer', // Include footer template
   });
 });
+
+// Set up routes for book operations
+app.get('/books', bookController.getAllBooks);
+app.get('/books/:id', bookController.getBookById);
+app.post('/books', bookController.createBook);
+app.put('/books/:id', bookController.updateBook);
+app.delete('/books/:id', bookController.deleteBook);
 
 // Start the server
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
