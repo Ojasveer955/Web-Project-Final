0440eb837df7ce4d155b9411b1a4e63ea867e141
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2024-12-01 21:04:33 +0530

    Fix

--- models/Book.js
+++ models/Book.js
@@ -6,6 +6,7 @@ const BookSchema = new mongoose.Schema({
     author: { type: String, required: true },
     category: { type: String, required: true },
     cover: { type: String, required: true },
+    price: { type: Number, required: true },
     description: { type: String, required: true },
 });
 
